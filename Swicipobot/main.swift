e73b31e582a47d5411e58433d9b4aeaaa6ac25db
eldardamirov <eldardamirov75@mail.ru>
2018-06-06 22:22:59 +0300

    Initial Commit

---
+++ Swicipobot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swicipobot
+//
+//  Created by Эльдар Дамиров on 06.06.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
